9ed2afcadb3b2dfecc74800c3af75f45f9a0f9ab
WarriorFromLongAgo <hubeixuegao@gmail.com>
2021-12-12 23:06:10 +0800

    测试 bat 脚本

--- git-push.bat
+++ git-push.bat
@@ -14,19 +14,11 @@ set /p declation=输入提交的commit信息:
 git commit -m "%declation%"
 echo;
  
-echo 将变更情况提交到远程自己分支：git push origin main
-git push origin main
-echo;
- 
-echo 切换主分支：git checkout main
-git checkout main
-echo;
- 
 echo 本地主分支拉取远程主分支：git pull origin main
 git pull origin main
 echo;
- 
-echo 将变更情况提交到远程主分支：git push origin main
+
+echo 将变更情况提交到远程自己分支：git push origin main
 git push origin main
 echo;
  
